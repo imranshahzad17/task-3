3d6b6f01a03be81f9afaff8aa4ff47882dd95fcb
imranshahzad17 <imranshahzad3454@gmail.com>
2024-03-06 23:52:40 +0500

    Done

---
+++ app.js
@@ -0,0 +1,46 @@
+// TOSS Task 1
+
+// let player1 = prompt("Enter Player name");
+// let player2 = prompt("Enter Player name");
+// let toss = Math.random()*2;
+// let change = Math.floor(toss);
+// if(change == 0){
+//       document.write(`${player1} You Won the toss And ${change}`);
+// }else{
+//     document.write(`${player2} You Won the toss And ${change}`);
+// }
+
+
+// ========================================================================================
+
+
+// Age calculate Task 2
+
+// let birthdate = new Date(prompt("Enter Your BirthDay                                   Example : 16 May 2004 "))
+// let today = new Date()
+// let Diff = today.getTime() - birthdate.getTime()
+// let day = Diff /(1000 * 60 * 60 *24 )
+// let str = ("Total Days");
+// let month = Diff / (1000 * 60 * 60 *24 * 30)
+// let str2 = ("Total Months");
+// let year = Diff / (1000 * 60 * 60 *24 * 30 * 12)
+// let str3 = ("Your Age");
+// console.log(Math.round(day),str ); 
+// console.log(Math.round(month),str2);
+// console.log(Math.round(year),str3);
+
+
+// ========================================================================================
+
+
+// Password Generator Task 3
+// let randomvalues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*?/"
+// let Password = "";
+// for(let i = 0 ; i < 10 ; i++){
+// let randomnumber = Math.floor(Math.random()*randomvalues.length)
+// Password += randomvalues[randomnumber]
+// }
+// console.log(Password);
+
+
+// ========================================================================================
